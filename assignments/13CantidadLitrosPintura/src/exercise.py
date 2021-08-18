763c34ad3af2846501b490f2d9254d162b2a71ac
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-17 17:42:53 -0700

    Update exercise.py

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,11 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+a = float(input('Area a pintar en metros: '))
+b = float(input('Rendimiento m2/L: '))
+c = a/b 
+math.ceil(c)
+print(c)
 
 
 if __name__ == '__main__':
