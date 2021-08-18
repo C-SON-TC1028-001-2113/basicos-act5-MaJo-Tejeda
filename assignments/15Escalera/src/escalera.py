3aa21f7e8788fa4278396af245f86e393e2e57ef
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-17 18:00:48 -0700

    Update escalera.py

--- assignments/15Escalera/src/escalera.py
+++ assignments/15Escalera/src/escalera.py
@@ -4,8 +4,8 @@ def main():
 h = float(input('Altura de la casa: '))
 a = int(input('Angulo en grados: ')) 
 
-
 largo = h / math.sin(a) 
+
 print('Largo de la escalera: ' + str(largo))
     
 if __name__ == '__main__':
